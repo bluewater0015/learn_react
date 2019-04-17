6d3ebe1d6a63a54cfdf6947f6b5ac3daabfb1823
wangxiao <wangxiao>
2019-04-17 09:35:40 +0800

    学习继承

--- src/test/Inherit/Inherit.js
+++ src/test/Inherit/Inherit.js
@@ -8,7 +8,7 @@ function Person() {
 Person.prototype.sayName = function(){
     return this.name;
 }
-
+var person = new Person();
 // var person = new Person();
 // console.log('Person.prototype',Person.prototype);
 // console.log('person',person.__proto__);
@@ -28,13 +28,95 @@ Son.prototype.saySonName = function() {
     return this.sonName;
 }
 var son = new Son();
-console.log('son---',son.sayName());  //晓晓
+//console.log('son---',son.sayName());  //天下
+//重写超类型中的方法
+Son.prototype.sayName = function() {
+    return false;
+}
+console.log('son---',son.sayName());  //false
 //console.log('Son.prototype.constructor',Son.prototype.constructor); //Person
 
 console.log('Object.Prototype.__proto__',Person.prototype.__proto__);
 //Object 是父级构造函数
 console.log('Object.Prototype.__proto__',Person.prototype.__proto__ ===  Object.prototype);
 console.log(Person.__proto__ === Function.prototype); //Function.prototype
+
+//2.确定原型和实例的关系
+// （1）instanceof,只要用这个操作符来测试实例与原型链中出现过的构造函数，结果就会返回true
+console.log('instanceof1',son instanceof Person); //true
+console.log('instanceof2',son instanceof Son);    //true
+console.log('instanceof3',son instanceof Object); //true
+// （2）isPrototypeOf() 只要原型链中出现的原型，都可以说是该原型链所派生的实例的原型
+console.log(Object.prototype.isPrototypeOf(son));  //true
+console.log(Person.prototype.isPrototypeOf(son));  //true
+console.log(Son.prototype.isPrototypeOf(son));     //true
+
+//通过原型链实现继承时，不能使用对象字面量创建原型方法，因为这样会重新原型链。
+function Person1() {
+    this.name = 'tianxia';
+}
+Person1.prototype.sayName = function() {
+    return this.name;
+}
+function Son1() {
+    this.sonName = 'xiaoxiao';
+}
+Son1.prototype = new Person1();
+Son1.prototype = {
+    getSonValue: function() {
+        return this.sonName;
+    }
+}
+var son1 = new Son1();
+console.log('123---',son1.__proto__ == Person1.prototype);
+//console.log(son1.sayName()); //Uncaught TypeError: son1.sayName is not a function
+
+//原型链的问题
+function Person2() {
+    this.colors = ['tianxia','www','aa'];
+}
+function Son2() {
+
+}
+//继承
+Son2.prototype = new Person2();
+var son2 = new Son2();
+son2.colors.push('111');
+console.log('son2.colors',son2.colors);
+var son22 = new Son2();
+console.log('son22.colors',son22.colors);
+
+//请问普通函数有原型对象没？
+function fun() {
+
+}
+console.log('fun',fun.prototype);
+
+//构造函数、原型、实例三者的关系
+function Person3() {
+    console.log('Person3-this',this);
+    this.name = 'tianxia';
+}
+var person3 = new Person3();
+Person3.prototype.sayName = function() {
+    console.log('这里的this是什么呢：',this);
+    console.log('this.name-Person3',this.name);
+}
+person3.sayName();
+console.log('person3',person3.__proto__ === Person3.prototype ); //true
+
+function Son3() {
+    console.log('son3-this',this);
+    this.sonName = 'Son3';
+}
+Son3.prototype = new Person3();
+console.log('Son3.prototype',Son3.prototype);
+var son3 = new Son3();
+
+Son3.prototype.saySonName = function() {
+    console.log('this.sonName',this);
+}
+son3.sayName(); //同时执行了父构造函数的方法
 
 class Inherit extends Component {
     render() {
